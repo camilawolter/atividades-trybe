5cf3c2021487e8a8dda859fd7772cea77aacc1fe
Camila Wolter <camila0wolter@gmail.com>
2022-05-13 15:15:29 -0300

    Adicionando o exercicio 3 do dojo

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/ex-fixar/script-dojo.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/ex-fixar/script-dojo.js
@@ -1,4 +1,4 @@
-//1. Descubra a idade mínima Escreva um algoritmo que recebe a idade de Marina, Silvia e Iza. Imprima no terminal uma mensagem informando a pessoa mais jovem no formato: -------------------------------------------------- "x é a pessoa mais jovem e possui y anos de idade" -------------------------------------------------- 
+//1
 
 const marina = 20;
 const silvia = 12;
@@ -16,7 +16,7 @@ if ( marina < silvia && marina < iza ) {
 }
 console.log(nome + " é a pessoa mais jovem e possui " + idade + " anos de idade");
 
-//2. Taxa Metabólica Basal Ana Laura é uma nutricionista e quer disponibilizar uma calculadora de BMR (Taxa Metabólica Basal) em seu site, vamos ajudá-la escrevendo um algoritmo. Ele recebe os seguintes dados: - age: número maior que 0 - sex: M ou F - weight: Em (kg) com número maior que 0 - height: Em (cm) com número maior que 0 A fórmula para homens: - (altura em centímetros x 6,25) + (peso em quilogramas x 9,99) – (idade x 4,92) + 5 A fórmula para mulheres: - (altura em centímetros x 6,25) + (peso em quilogramas x 9,99) – (idade x 4,92) – 161 Imprima no terminal o resultado do cálculo no formato: ----------------------------------- "A taxa metabólica basal é: x Kcal" -----------------------------------
+//2
 
 let idade = 23;
 let sex = "F";
@@ -40,6 +40,32 @@ switch (sex) {
 
 
 
-//3. Lanchonete da Trybe A Trybe abriu uma lanchonete e precisa criar um menu de opções para que as pessoas consigam escolher qual lanche maravilhoso irão pedir. Para isso escreva um algoritmo que recebe o número da opção escolhida conforme a tabela abaixo e imprima no terminal a mensagem de acordo com a opção escolhida. Tabela: "1 - Trybe Lanche Feliz" "2 - McTrybe" "3 - TrybeWooper" "4 - X-Trybe" "5 - Triplo Trybe com JS" Caso a pessoa escolha uma opção inexistente, imprima no terminal: ----------------------------------- "Ainda não possuímos esta opção :(" ----------------------------------- 
+//3
 
+let num = 1;
 
+switch (num) {
+    case 1:
+        lanche = "Trybe Lanche Feliz";
+        console.log("Seu pedido é " + lanche);
+        break;
+    case 2:
+        lanche = "McTrybe";
+        console.log("Seu pedido é " + lanche);
+        break;
+    case 3:
+        lanche = "TrybeWooper";
+        console.log("Seu pedido é " +lanche);
+        break;
+    case 4:
+        lanche = "X-Trybe";
+        console.log("Seu pedido é " + lanche);
+        break;
+    case 5:
+        lanche = "Triplo Trybe com JS";
+        console.log("Seu pedido é " + lanche);
+        break;
+    default:
+        console.log("Ainda não possuímos esta opção");
+        break;
+}
