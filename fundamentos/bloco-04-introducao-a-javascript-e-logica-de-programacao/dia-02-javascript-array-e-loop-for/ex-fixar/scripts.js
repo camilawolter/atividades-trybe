00d63b9775b8600f69c9a3fcd10484d841ae66e4
Camila Wolter <camila0wolter@gmail.com>
2022-05-13 10:49:55 -0300

    adicionando exercícios de fixação arrays/for

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/ex-fixar/scripts.js
@@ -0,0 +1,35 @@
+// EXERCÍCIO UM 
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+// EXERCÍCIO DOIS
+
+let indexOfPortfolio = menu[2];
+
+console.log(indexOfPortfolio);
+
+// EXERCÍCIO TRÊS
+
+let addMenu = menu.push('Contato')
+
+console.log(menu);
+
+//FOR
+// EXERCÍCIO UM
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (let index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index])
+};
+
+//FOR/OF
+// EXERCÍCIO UM
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let name of names) {
+    console.log(name)
+};
