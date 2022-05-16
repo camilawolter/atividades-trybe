dcaf26c6a953152749079cb1e439b50177c35654
Camila Wolter <camila0wolter@gmail.com>
2022-05-16 13:46:28 -0300

    Adicionando exercicio bonus até p 3

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/bonus/scripts.js
@@ -0,0 +1,39 @@
+// 1 - Imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
+
+// let simbolo = '*';
+// let n = 5;
+// let linha = '';
+
+// for (index= 0; index < n; index += 1) {
+//     linha = linha + simbolo
+// }
+//  for (index = 0; index < n; index += 1) {
+//     console.log(linha);
+//  }
+
+
+
+
+
+// 2 - Imprima na tela um triangulo feito de asteriscos de lado de tamanho n.
+
+// // let simbolo = '*';
+// // let n = 5;
+// // let linha = '';
+
+// for (index= 0; index < n; index += 1) {
+//     linha = linha + simbolo
+//     console.log(linha);
+// }
+
+// 3 - Inverter o lado do triangulo.
+
+let simbolo = '*';
+let n = 5;
+let linha = '';
+let posicao = n
+
+for (index= 0; index < n; index += 1) {
+    linha = linha + simbolo
+    console.log(linha);
+}
