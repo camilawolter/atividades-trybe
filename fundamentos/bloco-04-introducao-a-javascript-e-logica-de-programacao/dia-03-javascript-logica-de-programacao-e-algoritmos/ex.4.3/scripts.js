39f13dc9ebb2df160a1cd2c0582e45770b57fec3
Camila Wolter <camila0wolter@gmail.com>
2022-05-16 12:03:01 -0300

    Adicionando exercício 1

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/ex.4.3/scripts.js
@@ -0,0 +1,10 @@
+// 1 - Crie um algoritmo que retorne o fatorial de 10.
+
+let fatorial = 10;
+let resultado = fatorial
+
+for ( let index = 1; index < fatorial; index += 1) {
+    resultado *= index;
+}
+console.log(resultado);
+
