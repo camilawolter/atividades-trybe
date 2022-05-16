53a91279d454ec2067a1891acb93a4d258f9c6db
Camila Wolter <camila0wolter@gmail.com>
2022-05-16 19:21:21 -0300

    Adicionando exercício 3

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/bonus/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/bonus/scripts.js
@@ -33,7 +33,15 @@ let n = 5;
 let linha = '';
 let posicao = n
 
-for (index= 0; index < n; index += 1) {
-    linha = linha + simbolo
+for (linhaIndex= 0; linhaIndex <= n; linhaIndex += 1) {
+    for (let indexColuna = 0; indexColuna <= n; indexColuna += 1) {
+        if (indexColuna < posicao) {
+           linha =  linha + ' ';
+        } else {
+            linha = linha + simbolo;
+        }
+    }
     console.log(linha);
+    linha = '';
+    posicao -= 1;
 }
