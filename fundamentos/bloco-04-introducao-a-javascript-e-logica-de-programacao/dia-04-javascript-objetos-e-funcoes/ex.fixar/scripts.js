48ec1ed53f858df0c62aef6a104d9df9c26f53b0
Camila Wolter <camila0wolter@gmail.com>
2022-05-17 12:58:46 -0300

    adicionando restante dos exercicios para fixar

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/ex.fixar/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/ex.fixar/scripts.js
@@ -1,4 +1,4 @@
-// 1 objetos
+// 1. objetos
 
 let player = {
     name: 'Marta',
@@ -15,3 +15,54 @@ let player = {
 console.log('A jogadora ' + player.name + ' tem ' + player.age + ' anos de idade');
 console.log('A jogadora ' + player.name + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.');
 console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata');
+
+
+// 2. for/in
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+};
+  
+for (let index in names) {
+    console.log('Olá ' + names[index]);
+}
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+};
+
+for (let index in car)  {
+    console.log(index, car[index]);
+}
+
+// 3. funções 
+
+function maiorNum(primeiroNum, segundoNum) {
+    if (primeiroNum > segundoNum) {
+      return primeiroNum + ' é maior que ' + segundoNum;
+    } else if (segundoNum > primeiroNum) {
+      return segundoNum + ' é maior que ' + primeiroNum;
+    } else {
+      return 'Os números são iguais';
+    }
+}
+
+console.log(maiorNum(5, 80)); 
+console.log(maiorNum(18, -10)); 
+console.log(maiorNum(15, 15));
+
+let status = 'deslogado';
+
+function logarDeslogar() {
+  if (status === 'deslogado') {
+    status = 'logado';
+  } else {
+    status = 'deslogado';
+  }
+}
+
+console.log('O usuário está ' + status);
