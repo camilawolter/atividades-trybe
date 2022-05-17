7addcf2d62786580f709f6e01b919be41250be5f
Camila Wolter <camila0wolter@gmail.com>
2022-05-17 11:28:48 -0300

    adicionando exercício para fixar sobre objetos

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/ex.fixar/scripts.js
@@ -0,0 +1,17 @@
+// 1 objetos
+
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
+    medals: {
+        golden: 2,
+        silver: 3,
+    },
+};
+
+
+console.log('A jogadora ' + player.name + ' tem ' + player.age + ' anos de idade');
+console.log('A jogadora ' + player.name + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.');
+console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata');
