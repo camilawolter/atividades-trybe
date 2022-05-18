9acc23def1a2c19ab369a245cb595904681e319c
Camila Wolter <camila0wolter@gmail.com>
2022-05-18 11:29:54 -0300

    colocando exercício bônus

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/ex.bonus/script.js
@@ -0,0 +1,41 @@
+// BONUS
+
+//EX TRÊS
+
+const basket = [
+    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+    'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+    'Banana', 'Pera', 'Abacate', 'Uva',
+];
+
+const resultado = {};
+
+for (let index = 0; index < basket.length; index += 1) {
+    const frutas = basket[index];
+    if (!resultado[frutas]) resultado[frutas] = 0;
+    resultado[frutas] += 1;
+}
+console.log(resultado);
+
+const frase = [];
+for (frutas in resultado) {
+    let mensagem = `${resultado[frutas]} ${frutas}`;
+    if (resultado[frutas] > 1) 
+        mensagem += 's';
+    frase.push(mensagem);
+}
+console.log('Sua cesta possui: ' + frase.join(', '));
