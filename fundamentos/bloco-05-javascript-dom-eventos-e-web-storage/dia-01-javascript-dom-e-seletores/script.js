73d8ae12529f9d7d7c7dbf23c78791b2fc72e840
Camila Wolter <camila0wolter@gmail.com>
2022-05-19 21:41:26 -0300

    adicionando exercício 2 e 3

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
@@ -1,1 +1,23 @@
-document.getElementsByTagName('section')[0].innerText = "texto texto texto texto";
+// Exercício 1
+function textoParagraph () {
+    let texto = document.getElementsByTagName('p')[1];
+    texto.innerText = "texto texto texto texto texto texto texto texto texto texto texto texto";
+}
+textoParagraph();
+
+// Exercício 2
+function quadradoVerde () {
+    let quadradoAmarelo = document.getElementsByClassName('main-content')[0];
+    quadradoAmarelo.style.background = 'rgb(76,164,109)';
+}
+quadradoVerde();
+
+// Exercício 3
+function quadradoBranco () {
+    let quadradoVermelho = document.getElementsByClassName('center-content')[0];
+    quadradoVermelho.style.background = 'white';
+}
+quadradoBranco();
+
+
+document.getElementsByTagName('h1')[0].innerHTML = 'Exercício 5.1 - JavaScript';
