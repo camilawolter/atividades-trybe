9ad605ecd620e762fd31841e79365dd59601d570
Camila Wolter <camila0wolter@gmail.com>
2022-05-31 12:59:34 -0300

    resolução exercio fixar

---
+++ fundamentos/bloco-06-HTML-e-CSS-Forms-Flexbox-Responsivo/dia-01-HTML-e-CSS-Forms/ex.fixar.dois/script.js
@@ -0,0 +1,24 @@
+// SELETORES
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
+
+function getLink(evento){
+    evento.preventDefault();
+}
+HREF_LINK.addEventListener('click', getLink);
+
+
+function getCheck(evento) {
+    evento.preventDefault();
+}
+INPUT_CHECKBOX.addEventListener('click', getCheck);
+
+
+function getText(evento) {
+    const letra = evento.key
+    if (letra !== 'a') {
+        evento.preventDefault();
+    }
+}
+INPUT_TEXT.addEventListener('keypress', getText);
