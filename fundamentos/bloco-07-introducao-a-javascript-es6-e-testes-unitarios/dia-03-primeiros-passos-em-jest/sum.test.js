a5434e569dcaded7afb89cbbe525160423847048
Camila Wolter <camila0wolter@gmail.com>
2022-06-10 20:51:13 -0300

    exercício 1

---
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.test.js
@@ -0,0 +1,14 @@
+const sum = require('./sum.js');
+
+describe('sum', () => {
+    it('A soma de 4 e 5 é 9', () => {
+        expect(sum(4,5)).toEqual(9);
+    });
+    it('A soma de 0 e 0 é 0', () => {
+        expect(sum(0,0)).toEqual(0);
+    });
+    it('Se ocorre o erro se um dos parâmetros passados forem string', () => {
+        expect(() => sum(4,'5')).toThrowError();
+        expect(() => sum(4, '5')).toThrowError('parameters must be numbers');
+    })
+});
