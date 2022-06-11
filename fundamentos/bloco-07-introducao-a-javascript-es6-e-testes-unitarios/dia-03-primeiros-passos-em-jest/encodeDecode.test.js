e6b3b523d9c5d0bdb995a88f901e1066ef6c6f93
Camila Wolter <camila0wolter@gmail.com>
2022-06-11 13:27:24 -0300

    exercício 4

---
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/encodeDecode.test.js
@@ -0,0 +1,51 @@
+const {
+    encode,
+    decode
+} = require('./encodeDecode.js');
+
+describe('testando as funções encode e decode', () => {
+    it('se encode e decode é definada', () => {
+        expect(encode).toBeDefined();
+        expect(decode).toBeDefined();
+    });
+    it('se encode e decode são funções', () => {
+        expect(typeof encode).toEqual('function');
+        expect(typeof encode).toEqual('function');
+    });
+    it('função encode é convertida "a" para num 1', () => {
+        expect(encode('ama')).toEqual('1m1');
+    });
+    it('função encode é convertida "e" para num 2', () => {
+        expect(encode('dele')).toEqual('d2l2');
+    });
+    it('função encode é convertida "i" para num 3', () => {
+        expect(encode('ixi')).toEqual('3x3');
+    });
+    it('função encode é convertida "o" para num 4', () => {
+        expect(encode('ovo')).toEqual('4v4');
+    });
+    it('função encode é convertida "u" para num 5', () => {
+        expect(encode('nuvem')).toEqual('n5vem');
+    });
+    it('checa se o retorno da função tem o mesmo número de caracteres', () => {
+        expect(encode('trybe').length).toEqual(5);;
+    });
+    it('converte apenas o número 1 na vogal a', () => {
+        expect(decode('1n1')).toEqual('ana');
+    });
+    it('converte apenas o número 2 na vogal e', () => {
+        expect(decode('2l2')).toEqual('ele');
+    });
+    it('converte apenas o número 3 na vogal i', () => {
+        expect(decode('x3x3')).toEqual('xixi');
+    });
+    it('converte apenas o número 4 na vogal o', () => {
+        expect(decode('4v4')).toEqual('ovo');
+    });
+    it('converte apenas o número 5 na vogal u', () => {
+        expect(decode('n5')).toEqual('nu');
+    });
+    it('checa se o retorno da função tem o mesmo número de caracteres', () => {
+        expect(decode('trybe').length).toEqual(5);
+    });
+});
