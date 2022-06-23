6d20cb672466bf11224ca9971498bb686fd9964b
Camila Wolter <camila0wolter@gmail.com>
2022-06-23 11:27:22 -0300

    exercício três

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-intro-a-higher-order-functions/exercicios.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-intro-a-higher-order-functions/exercicios.js
@@ -26,5 +26,28 @@ const resultadoSorteio = (meuNumero, callback) => {
 
     return callback(meuNumero, numero) ? 'Parabéns você ganhou' : 'Tente novamente';
 };
-
 console.log(resultadoSorteio(5, numeroSorteio));
+
+//EXERCÍCIO TRÊS
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const comparaAnswers = (rightAnswers, studentAnswers) => {
+    if (rightAnswers === studentAnswers) {
+        return 1;
+    }
+    if (studentAnswers === 'N.A') {
+        return 0;
+    }
+    return -0.5
+};
+
+const totalPontos = (rightAnswers, studentAnswers, callback) => {
+    let contador = 0;
+    for (let index = 0; index <= rightAnswers.length; index += 1) {
+        const callbackReturn = callback(rightAnswers[index], studentAnswers[index]);
+            contador += callbackReturn;
+    }
+    return `Resultado: ${contador} pontos`;
+};
+console.log(totalPontos(RIGHT_ANSWERS, STUDENT_ANSWERS, comparaAnswers));
