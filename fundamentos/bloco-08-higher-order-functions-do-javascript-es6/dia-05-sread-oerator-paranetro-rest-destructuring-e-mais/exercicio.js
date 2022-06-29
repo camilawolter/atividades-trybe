b3d6955cbdb69f6f730a51455efa26c4b54fb490
Camila Wolter <camila0wolter@gmail.com>
2022-06-29 11:49:07 -0300

    adicionando exercício 6

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-sread-oerator-paranetro-rest-destructuring-e-mais/exercicio.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-sread-oerator-paranetro-rest-destructuring-e-mais/exercicio.js
@@ -78,3 +78,15 @@ const myList = [1, 2, 3];
 
 const swap = ([a, b, c]) => [c, b, a];
 console.log(swap(myList));
+
+// ex 6
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+const toObj = ([nome, marca, ano]) => ({
+    nome,
+    marca,
+    ano
+});
+console.log(toObj(palio));
