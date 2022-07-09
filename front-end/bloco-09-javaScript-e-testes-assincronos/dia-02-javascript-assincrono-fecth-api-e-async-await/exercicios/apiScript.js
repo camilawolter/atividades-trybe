3c27c25bb4883f174e896fdb5f9294782ce1c90d
Camila Wolter <camila0wolter@gmail.com>
2022-07-09 12:18:29 -0300

    exercício 1

---
+++ front-end/bloco-09-javaScript-e-testes-assincronos/dia-02-javascript-assincrono-fecth-api-e-async-await/exercicios/apiScript.js
@@ -0,0 +1,21 @@
+// apiScript.js
+const API_URL = 'https://icanhazdadjoke.com/';
+
+
+const fetchJoke = () => {
+    
+    const myObject = {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    };
+
+    const jokeContainer = document.getElementById('jokeContainer');
+
+    fetch(API_URL, myObject)
+        .then(response => response.json())
+        .then(data => jokeContainer.innerText = data.joke);
+};
+
+window.onload = () => fetchJoke();
--- front-end/bloco-09-javaScript-e-testes-assincronos/dia-02-javascript-assincrono-fecth-api-e-async-await/fetch.js
+++ front-end/bloco-09-javaScript-e-testes-assincronos/dia-02-javascript-assincrono-fecth-api-e-async-await/fetch.js
@@ -1,9 +1,43 @@
+const {
+    url
+} = require('inspector');
 const fetch = require('node-fetch');
 
-const fetchJoke = () => {
+// then e catch
+// const fetchJoke = () => {
+//     const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+//     fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.value))
+//     .catch((error) => console.log(`Deu ruim \n ${error}`));
+// }
+
+// fetchJoke();
+
+// async e await com tehn e catch
+// const fetchJoke = async () => {
+//     const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+//     const result = await fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => data.value)
+//     .catch((error) => `Deu ruim \n ${error}`);
+
+//     console.log(result);
+// }
+// fetchJoke();
+
+// async e await com try e catch
+const fetchJoke = async () => {
     const url = 'https://api.chucknorris.io/jokes/random?category=dev';
 
-    console.log(fetch(url));
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data.value);
+    } catch(error) {
+        console.log(`Deu ruim \n ${error}`);
+    }
 }
-
 fetchJoke();
