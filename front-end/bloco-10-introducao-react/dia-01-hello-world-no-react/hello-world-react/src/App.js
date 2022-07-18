3f2f7750e49eb06e4b3448d5541d64c92255b958
Camila Wolter <camila0wolter@gmail.com>
2022-07-18 11:48:30 -0300

    test react

---
+++ front-end/bloco-10-introducao-react/dia-01-hello-world-no-react/hello-world-react/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import HelloWorld from './helloWorld';
+
+function App() {
+  return (
+    // <span className="HelloWorld">Hello, world!!</span>
+    <HelloWorld />
+  );
+}
+
+export default App;
---
+++ front-end/bloco-10-introducao-react/dia-01-hello-world-no-react/hello-world-react/src/helloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class helloWorld extends React.Component {
+    render() {
+        return <span>Hello, world!</span>
+    }
+}
+
+export default helloWorld
