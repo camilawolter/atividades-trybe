95538b3d06ff1c1b096f7d0616d2f7f277b013fa
Camila Wolter <camila0wolter@gmail.com>
2022-10-27 10:21:19 -0300

    criando modulos node.js

---
+++ back-end/node.js-intro/exemplo-convert/brlValue.js
@@ -0,0 +1,5 @@
+const brl = 5.37;
+
+const usdToBrl = (valuesInUsd) => valuesInUsd * brl;
+
+module.exports = usdToBrl;
---
+++ back-end/node.js-intro/exemplo-convert/index.js
@@ -0,0 +1,6 @@
+const convert = require('./brlValue');
+
+const usd = 10;
+const brl = convert(usd);
+
+console.log(brl);
