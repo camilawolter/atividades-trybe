915ee90f4f63232af5bbdba47f43e5566293506c
Camila Wolter <camila0wolter@gmail.com>
2022-10-29 11:41:10 -0300

    exercicio 7

--- back-end/node.js-API-REST-com-Express/exercicio-criacao-servidor-Node-Express/src/app.js
+++ back-end/node.js-API-REST-com-Express/exercicio-criacao-servidor-Node-Express/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 const fs = require('fs').promises;
 const path = require('path');
@@ -32,6 +33,24 @@ app.get('/movies', async (req, res) => {
     const movies = await readFile();
 
     res.status(200).json(movies);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+app.post('/movies', async (req, res) => {
+  try {
+    const { movie, price } = req.body;
+    const movies = await readFile();
+    const newMovie = {
+      id: movies[movies.length - 1].id + 1,
+      movie,
+      price,
+    };
+    const allMovies = JSON.stringify([...movies, newMovie]);
+    await fs.writeFile(moviesPath, allMovies);
+
+    res.status(200).json(allMovies);
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
