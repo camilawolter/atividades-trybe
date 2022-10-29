7e879aa34d488d56cae93c0a6481d834cb50e858
Camila Wolter <camila0wolter@gmail.com>
2022-10-29 11:23:55 -0300

    exercicio 4

--- back-end/node.js-API-REST-com-Express/exercicio-criacao-servidor-Node-Express/src/app.js
+++ back-end/node.js-API-REST-com-Express/exercicio-criacao-servidor-Node-Express/src/app.js
@@ -2,4 +2,18 @@ const express = require('express');
 
 const app = express();
 
+const fs = require('fs').promises;
+const path = require('path');
+
+const moviesPath = path.resolve(__dirname, './movies.json');
+
+const readFile = async () => {
+  try {
+    const data = await fs.readFile(moviesPath);
+    return JSON.parse(data);
+  } catch (err) {
+    console.error(`Arquivo não pode ser lido: ${err}`);
+  }
+};
+
 module.exports = app;
