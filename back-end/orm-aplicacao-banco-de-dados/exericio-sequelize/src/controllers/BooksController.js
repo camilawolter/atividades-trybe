c881c0f032984df05904fd921d6c7fe810261c73
Camila Wolter <camila0wolter@gmail.com>
2022-11-28 17:36:55 -0300

    getbyid controller

--- back-end/orm-aplicacao-banco-de-dados/exericio-sequelize/src/controllers/BooksController.js
+++ back-end/orm-aplicacao-banco-de-dados/exericio-sequelize/src/controllers/BooksController.js
@@ -5,6 +5,16 @@ const getAll = async (req, res) => {
   res.status(200).json(books);
 };
 
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const book = BookService.getById(id);
+
+  if (!book) return res.status(404).json({ message: 'Book not found!' });
+
+  res.status(200).json(book);
+};
+
 module.exports = {
   getAll,
+  getById,
 }
