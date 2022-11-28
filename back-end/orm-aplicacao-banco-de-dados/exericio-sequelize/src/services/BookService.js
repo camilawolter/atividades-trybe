37742b5445140abc8641dc8dd6a3a1971b3f92a3
Camila Wolter <camila0wolter@gmail.com>
2022-11-28 17:34:38 -0300

    getbyid service

--- back-end/orm-aplicacao-banco-de-dados/exericio-sequelize/src/services/BookService.js
+++ back-end/orm-aplicacao-banco-de-dados/exericio-sequelize/src/services/BookService.js
@@ -3,8 +3,14 @@ const {Book} = require('../models');
 const getAll = async () => {
   const books = await Book.findAll();
   return books;
-}
+};
+
+const getById = async (id) => {
+  const book = await Book.findByPk(id);
+  return book;
+};
 
 module.exports = {
   getAll,
-}
+  getById,
+};
