f7b2176498a15d6b2fd739d52c2e87621ef15c22
Camila Wolter <camila0wolter@gmail.com>
2022-11-28 17:18:12 -0300

    vinculo GET /books

---
+++ back-end/orm-aplicacao-banco-de-dados/exericio-sequelize/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const BooksController = require('./controllers/BooksController'); 
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/books', BooksController.getAll);
+
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
