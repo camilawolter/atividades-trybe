95db44be3d8183cd3e3067c08bd7a294f86f8ea7
Camila Wolter <camila0wolter@gmail.com>
2023-03-21 18:47:39 -0300

    feat: calculando áreas

---
+++ ciencia-da-computação/intro-python/exercicio-fixacao/area.py
@@ -0,0 +1,21 @@
+PI = 3.14
+
+
+def square(side):
+    "Calculate area of square."
+    return side * side
+
+
+def rectangle(length, width):
+    "Calcutate area of rectangle."
+    return length * width
+
+
+def circle(radius):
+    "Calculate area of circle"
+    return PI * radius * radius
+
+
+print("Área do quadrado:", square(10))
+print("Área do quadrado:", rectangle(2, 2))
+print("Área do quadrado:", circle(3))
--- ciencia-da-computação/intro-python/exercicios.py
+++
@@ -1,1 +0,0 @@
-camila = 'pessoa estudante'
